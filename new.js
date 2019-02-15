aeb2106a6eb2f5d88c922dd10d2cb37645670b68
neeraj <neeeraj2599@gmail.com>
2019-02-15 22:33:50 +0530

    some info about new keywords

---
+++ new.js
@@ -0,0 +1,14 @@
+function fruit(color , taste){
+    this.color =color ;
+    this.taste=taste;
+
+}
+let mango ={
+    color: "yellow",
+    taste:"sweet"
+}
+let orange=new fruit("orange","sour");
+let apple=new fruit ("red" ,"sweet");
+console.log(apple);
+console.log(orange);
+console.log(mango);
